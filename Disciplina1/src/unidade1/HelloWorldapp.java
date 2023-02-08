8320b77038a2b6ce96d3e35029d34c4e2f5a9456
Rubens Lima <rubens0903@gmail.com>
2023-02-08 12:39:27 -0300

    Commit inicial

---
+++ Disciplina1/src/unidade1/HelloWorldapp.java
@@ -0,0 +1,13 @@
+package unidade1;
+
+import javax.swing.JOptionPane;
+
+public class HelloWorldapp {
+
+	public static void main(String[] args) {
+		//System.out.println("Hello World!");
+		JOptionPane.showMessageDialog(null, "Hello World!");
+
+	}
+
+}
